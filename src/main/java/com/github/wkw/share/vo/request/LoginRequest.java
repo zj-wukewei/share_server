f940bf523f438c053a17cbf0bacbe409ef32716a
zj-wukewei <565389849@qq.com>
2018-08-08 17:24:05 +0800

    登录接口加入数据检验

--- src/main/java/com/github/wkw/share/vo/request/LoginRequest.java
+++ src/main/java/com/github/wkw/share/vo/request/LoginRequest.java
@@ -1,7 +1,5 @@
 package com.github.wkw.share.vo.request;
 
-import org.hibernate.validator.constraints.Length;
-
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -14,7 +12,7 @@ public class LoginRequest {
     @Pattern(regexp = "^1(3|4|5|7|8)\\d{9}$", message = "手机号码格式错误")
     @NotBlank(message = "手机号码不能为空")
     private String mobile;
-    @Length(min = 6, max = 12, message = "密码为6到12位")
+    @NotBlank(message = "密码不能为空")
     private String password;
 
     public String getMobile() {
